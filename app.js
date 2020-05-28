2c8a7c7078dd350fdfdfaf99d983b062556f71b8
diewland <diewland@gmail.com>
2020-05-28 23:20:06 +0700

    query shop-name from api when add place

--- app.js
+++ app.js
@@ -13,20 +13,31 @@ let Db = {
     return Disk.get(APP_TITLE, []);
   },
 
-  add: (url) => {
+  add: (url, app_id, shop_id, callback) => {
     let data = Db.list();
     let row = data.filter(r => r.url == url)[0];
-    if (row) { // update timestamp
+    // update timestamp
+    if (row) {
+      // update data
       row.timestamp = ts();
+      // save & callback
+      Disk.set(APP_TITLE, data);
+      callback();
     }
-    else { // add new row
-      data.push({
-        title: '---TODO---', // TODO
-        url: url,
-        timestamp: ts(),
+    // add new row
+    else {
+      Thaichana.get_shop_info (app_id, shop_id, info => {
+        // update data
+        data.push({
+          title: info.shopName,
+          url: url,
+          timestamp: ts(),
+        });
+        // save & callback
+        Disk.set(APP_TITLE, data);
+        callback();
       });
     }
-    return Disk.set(APP_TITLE, data);
   },
 
 };
