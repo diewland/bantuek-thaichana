1e8ba59422f4a2a7279888c14070e55e04b95281
yuwat <yuwat@mfec.co.th>
2021-01-10 23:14:17 +0700

    fix #18 ignore back button on main screen

---
+++ ignore-back.js
@@ -0,0 +1,8 @@
+//
+// https://stackoverflow.com/a/34337617/466693
+//
+history.pushState(null, document.title, location.href);
+window.addEventListener('popstate', function (event)
+{
+  history.pushState(null, document.title, location.href);
+});
